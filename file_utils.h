ab3735ee1a81e467f2cf0bf44b450296efb1d94d
Charbelrobertelias <eliascharb@cy-tech.fr>
2024-05-19 17:00:39 +0200

    Create file_utils.h

---
+++ file_utils.h
@@ -0,0 +1,9 @@
+#ifndef FILE_UTILS_H
+#define FILE_UTILS_H
+
+#include "performance.h"
+
+void sauvegarderPerformance(char* nom, Performance* performance);
+void lireHistorique(char* nom);
+
+#endif
