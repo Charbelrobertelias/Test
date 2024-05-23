4e86cebf15c4374a9776d8febd545d10639f978b
Charbelrobertelias <eliascharb@cy-tech.fr>
2024-05-23 13:29:56 +0200

    Update file_utils.h

--- file_utils.h
+++ file_utils.h
@@ -1,9 +1,12 @@
+
 #ifndef FILE_UTILS_H
 #define FILE_UTILS_H
 
 #include "performance.h"
 
+// Sauvegarde une performance dans un fichier
 void sauvegarderPerformance(char* nom, Performance* performance);
+// Lit l'historique des performances d'un athlète
 void lireHistorique(char* nom);
 
 #endif
