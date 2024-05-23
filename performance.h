dcf648a8a3e54c431b7bd68ef9cf64b07d163789
Charbelrobertelias <eliascharb@cy-tech.fr>
2024-05-23 13:26:18 +0200

    Update performance.h

--- performance.h
+++ performance.h
@@ -7,6 +7,7 @@
 #include <time.h>
 #include "athlete.h"
 
+// Structure représentant une performance
 typedef struct {
     char date[11];
     char epreuve[50];
@@ -14,14 +15,19 @@ typedef struct {
     int positionRelais; // -1 si ce n'est pas une épreuve de relais
 } Performance;
 
+// Crée une performance
 Performance* creerPerformance(char* date, char* epreuve, float temps, int positionRelais);
+// Affiche une performance
 void afficherPerformance(Performance* performance);
+// Ajoute un nouvel entraînement
 void ajouterEntrainement();
+// Consulte l'historique des entraînements
 void consulterHistorique();
-void consulterStats();
+// Valide le format d'une date
 int validerDate(char* date);
+// Valide le format d'un temps
 int validerTemps(float temps);
+// Valide le nom d'une épreuve
 int validerEpreuve(char* epreuve);
-int validerGenre(char* genre);
 
 #endif
