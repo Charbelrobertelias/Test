c5cdf2a30bc05377e9e7de20932c034fc0ba8caa
Charbelrobertelias <eliascharb@cy-tech.fr>
2024-05-23 13:31:21 +0200

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,7 +1,11 @@
 #ifndef STATS_H
 #define STATS_H
 
+// Affiche les statistiques des performances d'un athlète pour une épreuve donnée
 void afficherStats(char* nom, char* epreuve);
+// Affiche les trois meilleurs athlètes pour chaque épreuve et les envoie aux JO
 void envoyerAuxJO();
+// Affiche la progression d'un athlète pour une même épreuve entre deux dates
+void afficherProgression(char* nom, char* epreuve, char* date1, char* date2);
 
 #endif
